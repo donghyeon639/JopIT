e98cfc82ce4f5257f31b5f155592fbf7bbff05a8
donghyun <dlehdgus213@gmail.com>
2026-05-07 16:11:24 +0900

    인증 config fix

--- src/main/java/com/main/jobit/security/SecurityConfig.java
+++ src/main/java/com/main/jobit/security/SecurityConfig.java
@@ -63,7 +63,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://job-it.site",
+                "https://www.job-it.site"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
